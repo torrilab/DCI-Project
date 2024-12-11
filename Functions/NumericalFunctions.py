a8e50232bfc6ef0a8a9c13c3c2d6f41472e09d0f
Abraham Roseman <abrahamroseman@dhcp-168-105-253-75.wireless.manoa.hawaii.edu>
2024-12-10 16:48:30 -1000

    “12/10/24”

---
+++ Functions/NumericalFunctions.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# #How to Import to Code Document
+# import sys
+# dir='/mnt/lustre/koa/koastore/torri_group/air_directory/Project/'
+# path=dir+'../Functions'
+# sys.path.append(path)
+# from NumericalFunctions import *
+
+
+# In[ ]:
+
+
+def Ddt(f,dt):
+    # dt=(data['time'][1]-data['time'][0]).item()/1e9
+    _ddt=np.zeros_like(f)
+    _ddt[1:-1] = (f[2:] - f[:-2]) / (dt)
+    return _ddt
+
+def Ddz(f,dz):    
+    _ddz=np.zeros_like(f)
+    _ddz[:, 1:-1] = (f[:, 2:] - f[:, :-2]) / (2 * dz)
+    _ddz[:, 0] = (f[:, 1] - f[:, 0]) / dz  # Forward difference 
+    _ddz[:, -1] = (f[:, -1] - f[:, -2]) / dz  # Backward difference 
+    return _ddz
+
+def DdzStretch(f):
+    #f must be interpolated to cell centers
+    dz=np.diff(data['zf'].values)
+    dz=dz.copy()[np.newaxis, :, np.newaxis, np.newaxis]
+    
+    ddz=np.zeros_like(f)
+    ddz[:, 1:-1] = (f[:, 2:] - f[:, :-2]) / (2 * dz[:, 1:-1])
+    ddz[:, 0] = (f[:, 1] - f[:, 0]) / dz[:, 0]  # Forward difference 
+    ddz[:, -1] = (f[:, -1] - f[:, -2]) / dz[:, -1]  # Backward difference 
+    return ddz
+
+def Ddy(f,dy):    
+    _ddy=np.zeros_like(f)
+    _ddy[:, :, 1:-1] = (f[:, :, 2:] - f[:, :, :-2]) / (2 * dy)
+    _ddy[:, :, 0] = (f[:, :, 1] - f[:, :, 0]) / dy  # Forward difference 
+    _ddy[:, :, -1] = (f[:, :, -1] - f[:, :, -2]) / dy  # Backward difference 
+    return _ddy
+
+def Ddx(f,dx):    
+    _ddx=np.zeros_like(f)
+    _ddx[:, :, :, 1:-1] = (f[:, :, :, 2:] - f[:, :, :, :-2]) / (2 * dx)
+    _ddx[:, :, :, 0] = (f[:, :, :, 1] - f[:, :, :, 0]) / dx  # Forward difference 
+    _ddx[:, :, :, -1] = (f[:, :, :, -1] - f[:, :, :, -2]) / dx  # Backward difference 
+    return _ddx
+
+def LaplacianHoriz(f,dx,dy):
+    # Initialize the second derivatives arrays with zeros, same shape as f
+    d2f_dx2 = np.zeros_like(f)
+    d2f_dy2 = np.zeros_like(f)
+    
+    # Second derivatives using central differences
+    d2f_dx2[:, :, 1:-1, :] = (f[:, :, :-2, :] - 2 * f[:, :, 1:-1, :] + f[:, :, 2:, :]) / dx**2
+    d2f_dy2[:, :, :, 1:-1] = (f[:, :, :, :-2] - 2 * f[:, :, :, 1:-1] + f[:, :, :, 2:]) / dy**2
+    
+    # Combine to reconstruct the Laplacian (RHS)
+    out = d2f_dx2 + d2f_dy2
+    return out
+
+def Laplacian3D(f, dx, dy, dz):
+    #f must be provided at a specific 
+    
+    # Initialize the second derivatives arrays with zeros, same shape as f
+    d2f_dz2 = np.zeros_like(f)
+    d2f_dy2 = np.zeros_like(f)
+    d2f_dx2 = np.zeros_like(f)
+    
+    # Second derivatives using central differences
+    d2f_dz2[:, 1:-1, :, :] = (f[:, :-2, :, :] - 2 * f[:, 1:-1, :, :] + f[:, 2:, :, :]) / dz**2
+    d2f_dy2[:, :, 1:-1, :] = (f[:, :, :-2, :] - 2 * f[:, :, 1:-1, :] + f[:, :, 2:, :]) / dy**2
+    d2f_dx2[:, :, :, 1:-1] = (f[:, :, :, :-2] - 2 * f[:, :, :, 1:-1] + f[:, :, :, 2:]) / dx**2
+    
+    # Combine to reconstruct the Laplacian (RHS)
+    out = d2f_dx2 + d2f_dy2 + d2f_dz2
+    return out
+
+def Laplacian3DStretch(f, dx, dy):
+    # Initialize the second derivatives arrays with zeros, same shape as f
+    #f must be interpolated to cell centers
+    #f must be an array array with f for face and h for center (e.g. zf/zh)
+    dz=np.diff(data['zf'].values)
+    dz=dz.copy()[np.newaxis, :, np.newaxis, np.newaxis]
+    
+    # Initialize the second derivatives arrays with zeros, same shape as f
+    d2f_dz2 = np.zeros_like(f)
+    d2f_dy2 = np.zeros_like(f)
+    d2f_dx2 = np.zeros_like(f)
+    
+    # Second derivatives using central differences
+    d2f_dz2[:, 1:-1, :, :] = (f[:, :-2, :, :] - 2 * f[:, 1:-1, :, :] + f[:, 2:, :, :]) / dz[:,1:-1]**2
+    d2f_dy2[:, :, 1:-1, :] = (f[:, :, :-2, :] - 2 * f[:, :, 1:-1, :] + f[:, :, 2:, :]) / dy**2
+    d2f_dx2[:, :, :, 1:-1] = (f[:, :, :, :-2] - 2 * f[:, :, :, 1:-1] + f[:, :, :, 2:]) / dx**2
+    
+    # Combine to reconstruct the Laplacian (RHS)
+    out = d2f_dx2 + d2f_dy2 + d2f_dz2
+    return out
+
+# #TESTING
+# ######################################
+# import numpy as np
+# f = np.random.random((4, 4, 4, 4))
+# Ddt(f,1)
+# Ddz(f,1)
+# Ddy(f,1)
+# Ddx(f,1)
+# HorizLaplacian(f,1,1)
+# Laplacian3D(f,1,1,1)
+
+# import xarray as xr
+# dir='/mnt/lustre/koa/koastore/torri_group/air_directory/'
+# data=xr.open_dataset(dir+'/cm1r20.3/run/cm1out_test7tundra-7_062217.nc') #***
+# f=data['w'].interp(zf=data['zh']).data
+# DdzStretch(f)
+# Laplacian3DStretch(f,1,1)
+
+
+# In[ ]:
+
+
+#Averaging and Slicing Functions
+def HorizAvg_zt(f): 
+    out=np.mean(f, axis=(2,3)) #takes horizontal average leaving f(t,z)
+    return out
+def VertProfile_z(f): 
+    out=np.mean(f, axis=(0,2,3)) #takes horizontal + time average leaving f(z)
+    return out
+def HorizProfile_txy(f): 
+    out=np.mean(f, axis=(1)) #takes horizontal + time average leaving f(z)
+    return out    
+def Slice(type,f,ind):
+    if type=='t':
+        out=f[ind]
+    if type=='z':
+        out=f[:,ind]
+    if type=='y':
+        out=f[:, :, ind]
+    if type=='x':
+        out=f[:, :, :, ind]
+    return out
+    
+# #TESTING
+# ######################################
+# import numpy as np
+# f = np.random.random((4, 4, 4, 4))
+# HorizAvg_zt(f)
+# VertProfile_z(f)
+# HorizProfile_txy(f)
+# Slice('t',f,2)
+# Slice('z',f,2)
+# Slice('y',f,2)
+# Slice('x',f,2)
+
---
+++ Functions/PlottingFunctions.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Importing Packages
+import numpy as np
+import xarray as xr
+
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import matplotlib.ticker as ticker
+import matplotlib.cm as cm
+from matplotlib.colors import Normalize
+from matplotlib.ticker import MaxNLocator
+from matplotlib.ticker import ScalarFormatter
+import matplotlib.gridspec as gridspec
+
+
+# In[160]:
+
+
+# #TESTING
+# ######################################
+dir='/mnt/lustre/koa/koastore/torri_group/air_directory/'
+data=xr.open_dataset(dir+'/cm1r20.3/run/cm1out_test7tundra-7_062217.nc') #***
+f=data['w'].interp(zf=data['zh']).data
+
+
+# In[31]:
+
+
+def VerticalContour(f,units):
+    plt.contourf(f.T)
+    plt.xlabel('time');plt.ylabel('z (km)')
+
+    #Fixing Vertical Ticks
+    tick_inds=np.arange(0,len(data['zh'].values),2)
+    plt.yticks(ticks=tick_inds, labels=np.round(data['zh'].values[tick_inds], 1));
+    plt.yticks(ticks=tick_inds, labels=np.round(data['zh'].values[tick_inds], 1));
+    
+    #Scientific Notation
+    if units == "sci":
+        print('hrey')
+        from matplotlib.ticker import ScalarFormatter
+        formatter = ScalarFormatter(useMathText=True)
+        formatter.set_powerlimits((-3, 3))
+        plt.gca().xaxis.set_major_formatter(formatter)  
+        
+
+
+# In[254]:
+
+
+def VertContour(f, units, xax, ax=None, ny=None, nx=None):
+    # Create an axis if one is not provided
+    if ax is None:
+        fig, ax = plt.subplots()
+    if ny is None: ny=2
+
+    # Plot the contour on the specified axis
+    if xax=='t':
+        c = ax.contourf(f.T);
+    else:
+        c = ax.contourf(f);
+
+    # Scientific Notation
+    if units == "sci":
+        from matplotlib.ticker import ScalarFormatter
+        formatter = ScalarFormatter(useMathText=True)
+        formatter.set_powerlimits((-3, 3))
+        ax.xaxis.set_major_formatter(formatter)
+
+    # Set labels
+    if xax=='t':
+        if nx is None: nx=1
+        ax.set_xlabel('time')
+    elif xax=='y':
+        ax.set_xlabel('y (km)')
+    elif xax=='x':
+        ax.set_xlabel('x (km)')
+
+    # Fixing Vertical Ticks
+    tick_inds = np.arange(0, len(data['zh'].values), ny)
+    ax.set_yticks(tick_inds)
+    ax.set_yticklabels(np.round(data['zh'].values[tick_inds], 1))
+
+    if xax=='y':
+        if nx is None: nx=5
+        tick_inds = np.arange(0, len(data['yh'].values), nx)
+        ax.set_xticks(tick_inds)
+        ax.set_xticklabels(np.round(data['yh'].values[tick_inds], 1))
+        
+    elif xax=='x':
+        if nx is None: nx=60
+        tick_inds = np.arange(0, len(data['xh'].values), nx)
+        ax.set_xticks(tick_inds)
+        ax.set_xticklabels(np.round(data['xh'].values[tick_inds], 1))
+        
+    # plt.close(fig)
+    return ax
+    
+def HorizContour(f, units, ax=None, nx=None, ny=None):
+    # Create an axis if one is not provided
+    if ax is None:
+        fig, ax = plt.subplots()
+    if nx is None: nx=60
+    if ny is None: ny=5
+
+    # Plot the contour on the specified axis
+    c = ax.contourf(f);
+
+    # Scientific Notation
+    if units == "sci":
+        from matplotlib.ticker import ScalarFormatter
+        formatter = ScalarFormatter(useMathText=True)
+        formatter.set_powerlimits((-3, 3))
+        ax.xaxis.set_major_formatter(formatter)
+
+    # Set labels
+    ax.set_xlabel('x (km)')
+    ax.set_ylabel('y (km)')
+
+    # Fixing Horizontal Ticks
+    tick_inds = np.arange(0, len(data['xh'].values), nx)
+    ax.set_xticks(tick_inds)
+    ax.set_xticklabels(np.round(data['xh'].values[tick_inds], 1))
+    
+    tick_inds = np.arange(0, len(data['yh'].values), ny)
+    ax.set_yticks(tick_inds)
+    ax.set_yticklabels(np.round(data['yh'].values[tick_inds], 1))
+    
+    # plt.close(fig)
+    return ax
+
+
+# In[258]:
+
+
+# #TESTING
+# ######################################
+# VertContour(f[:,:,10,10], units="sci", xax='t')
+# VertContour(f[10,:,:,10], units="sci", xax='y',nx=10)
+# VertContour(f[10,:,10,:], units="sci", xax='x')
+# HorizContour(f[10,10,:,:], units="sci")
+
+
+# In[221]:
+
+
+# #How To Plot Multiple GridSpec Subplots
+# fig = plt.figure(figsize=(10, 5))
+# gs = gridspec.GridSpec(1, 2, figure=fig)
+
+# # Create first subplot
+# ax1 = fig.add_subplot(gs[0, 0])
+# VertContour(f[:,:,10,10], units="sci", ax=ax1)
+
+# # Create second subplot
+# ax2 = fig.add_subplot(gs[0, 1])
+# VertContour(f[:,:,10,10], units="sci", ax=ax2)
+
